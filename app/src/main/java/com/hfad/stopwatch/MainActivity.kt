fad2b8945516e575cd2871d7812069cf96e151b0
itshash01 <eudelrainfall@gmail.com>
2022-12-09 22:55:07 +0400

    StopWatch project

--- app/src/main/java/com/hfad/stopwatch/MainActivity.kt
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -1,23 +1,52 @@
 package com.hfad.stopwatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var stopwatch: Chronometer  // the stop watch
     var running = false    // is the stop watch running?
     var offset: Long = 0   // the base offset for the stopwatch
+    // Add key strings for use with the bundle.
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // Get a reference for the stopwatch
-        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
-        // The button starts the stopwatch i its not running
+        stopwatch = findViewById(R.id.stopwatch)
+        // Restore the previous state.
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }else setBaseTime()
+        }
+        // what the app does when another activity makes it invisible.
+        fun onPause() {
+            super.onPause()
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+            }
+        }
+
+        fun onResume() {
+            super.onResume()
+            if (running) {
+                setBaseTime()
+                stopwatch.start()
+                offset = 0
+            }
+        }
+        // The button starts the stopwatch if its not running
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener {
             if (!running) {
@@ -26,7 +55,6 @@ class MainActivity : AppCompatActivity() {
                 running = true
             }
         }
-
         // The pause button pauses the stopwatch if its running
         val pauseButton = findViewById<Button>(R.id.pause_button)
         pauseButton.setOnClickListener {
@@ -36,7 +64,6 @@ class MainActivity : AppCompatActivity() {
                 running = false
             }
         }
-
         // The reset button set the offset and stopwatch to zero
         val resetButton = findViewById<Button>(R.id.reset_button)
         resetButton.setOnClickListener {
@@ -44,11 +71,17 @@ class MainActivity : AppCompatActivity() {
             setBaseTime()
         }
     }
+    // magnetoresistance activity.
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
     // Update te stopwatch base time, allowing for any offset
     fun setBaseTime() {
         stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
-
     // Record the offset
     fun saveOffset() {
         offset = SystemClock.elapsedRealtime() - stopwatch.base
